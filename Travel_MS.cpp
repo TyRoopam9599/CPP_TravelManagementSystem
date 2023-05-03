332503f1ca3013c9d42b21a39bcba7e1322ae085
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-05-03 21:18:30 +0530

    Travel Management System

--- Travel_MS.cpp
+++ Travel_MS.cpp
@@ -53,6 +53,7 @@ class Cabs{
     public:
     int cabChoice;
     int kilometers;
+    string bookedCab ;
     static float price;
 
     void cabDetails(){
@@ -76,33 +77,43 @@ class Cabs{
         switch(cabChoice){
             case 1:
                 price= kilometers*10.0 ;
+                bookedCab="Micro" ;
                 break ;
             case 2:
                 price= kilometers*15.0;
+                bookedCab="Mini" ;
                 break ;
             case 3:
                 price= kilometers*20.0 ;
+                bookedCab="Prime";
                 break ;
             case 4:
                 price= kilometers*25.0;
+                bookedCab="SUV";
                 break ;
             case 5:
                 price= kilometers*30.0 ;
+                bookedCab="Sedan";
                 break ;
             case 6:
                 price= kilometers*40.0;
+                bookedCab="Luxury";
                 break ;
             case 7:
                 price= kilometers*12.0 ;
+                bookedCab="Buses";
                 break ;
             case 8:
                 price= kilometers*8.0;
+                bookedCab="Tempo Traveller";
                 break ;
             case 9:
                 price= kilometers*50.0 ;
+                bookedCab="Airport Transfer";
                 break ;
             case 10:
                 price= kilometers*60.0;
+                bookedCab="Outstation";
                 break ;
         }
         cout<<"Your total bill will be: "<<price<<endl ;
@@ -122,14 +133,215 @@ class Cabs{
 };
 
 class Booking{
+    public:
+    int chooseHotel;
+    int Package;
+    int gotoMenu;
+    string bookedHotel;
+    static float hotelPrice ;
 
+    void hotels(){
+        string hotelName[10] = {"Taj Palace", "Hotel Taj", "Royal Hotel", "Empire Hotel", "Kapil Hotel", "Hotel Raj", "Tyagi's Hotel", "London Hotel", "Paris Hotel", "Uk Hotel"};
+        float hotelprice[10] = {1000,2000,3000,1500,2500,2200,1700,2600,3100,4000} ;
+
+        for(int i=0;i<10;i++){
+            cout<<i+1<<" "<<hotelName[i]<<"-->"<<hotelprice[i]<<endl ;
+        }
+        char choice ;
+        cout<<"Currently we have 10 hotels in our database. You can choose any one of them\n";
+        cin>>chooseHotel;
+        switch(chooseHotel){
+            case 1:
+                cout<<"You have selected "<<hotelName[0]<<" for your stay. The price of this hotel is "<<hotelprice[0]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[0] ;
+                    bookedHotel = hotelName[0] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 2:
+                cout<<"You have selected "<<hotelName[1]<<" for your stay. The price of this hotel is "<<hotelprice[1]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[1] ;
+                    bookedHotel = hotelName[1] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 3:
+                cout<<"You have selected "<<hotelName[2]<<" for your stay. The price of this hotel is "<<hotelprice[2]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[2] ;
+                    bookedHotel = hotelName[2] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 4:
+                cout<<"You have selected "<<hotelName[3]<<" for your stay. The price of this hotel is "<<hotelprice[3]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[3] ;
+                    bookedHotel = hotelName[3] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 5:
+                cout<<"You have selected "<<hotelName[4]<<" for your stay. The price of this hotel is "<<hotelprice[4]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[4] ;
+                    bookedHotel = hotelName[4] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            
+            case 6:
+                cout<<"You have selected "<<hotelName[5]<<" for your stay. The price of this hotel is "<<hotelprice[5]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[5] ;
+                    bookedHotel = hotelName[5] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 7:
+                cout<<"You have selected "<<hotelName[6]<<" for your stay. The price of this hotel is "<<hotelprice[6]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[6] ;
+                    bookedHotel = hotelName[6] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 8:
+                cout<<"You have selected "<<hotelName[7]<<" for your stay. The price of this hotel is "<<hotelprice[7]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[7] ;
+                    bookedHotel = hotelName[7] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 9: 
+                cout<<"You have selected "<<hotelName[8]<<" for your stay. The price of this hotel is "<<hotelprice[8]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[8] ;
+                    bookedHotel = hotelName[8] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            case 10: 
+                cout<<"You have selected "<<hotelName[9]<<" for your stay. The price of this hotel is "<<hotelprice[9]<<endl;
+                cout<<"Do you want tobook this hotel? (Y/N): ";
+                cin>>choice ;
+                if(choice=='Y'||choice=='y'){
+                    cout<<"Your hotel is booked\n";
+                    hotelPrice = hotelprice[9] ;
+                    bookedHotel = hotelName[9] ;
+                    cout<<"Goto menu to take the receipt\n";
+                }
+                else{
+                    cout<<"Thank you for using our service\n";
+                    hotels() ;
+                }
+                break ;
+            default:
+                cout<<"Invalid Input"<<endl ;
+                hotels() ;
+                break ;
+        }
+    }
 };
 
-class Receipt{
-
+class Receipt : public Booking, Cabs, Customer{         //Multiple Inheritance
+    public:
+    void printBill(){
+        ofstream outf("Receipt.txt");
+        {
+            outf<<"---------------------------------------------"<<endl;
+            outf<<"-------------------Receipt-------------------"<<endl;
+            outf<<"---------------------------------------------"<<endl<<endl;
+            outf<<"Customer ID: "<<Customer::custID<<endl<<endl;
+            outf<<"---------------------------------------------"<<endl;
+            outf<<"-------------------Booking-------------------"<<endl;
+            outf<<"---------------------------------------------"<<endl<<endl;
+            cout<<"Hotel Name: "<<Booking::bookedHotel<<endl ;
+            cout<<"Hotel Price: "<<Booking::hotelPrice<<endl ;
+            outf<<"---------------------------------------------"<<endl;
+            outf<<"--------------------Cabs---------------------"<<endl;
+            outf<<"---------------------------------------------"<<endl<<endl;
+            outf<<"Cab Name: "<<Cabs::bookedCab<<endl;
+            outf<<"Cab Price: "<<Cabs::price<<endl<<endl;
+            outf<<"---------------------------------------------"<<endl;
+            outf<<"--------------------Total--------------------"<<endl;
+            outf<<"---------------------------------------------"<<endl<<endl;
+            outf<<"Total Price: "<<Booking::hotelPrice + Cabs::price<<endl ;
+            outf<<"---------------------------------------------"<<endl;
+            outf<<"------------------Thank You------------------"<<endl;
+            outf<<"---------------------------------------------"<<endl<<endl;
+        }
+        outf.close() ;
+    }
 };
 
 int main(){
-
+    Customer obj = Customer();
     return 0 ;
 }
